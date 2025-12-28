a73d64f5a8a619213e519bfca7e594c1f4a82d04
Sarfenaz Naz <sarfenaz.naz@gmail.com>
2025-12-28 12:27:11 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; url=public/index.php">
+    <title>Redirecting...</title>
+    <script>
+        window.location.href = "public/index.php";
+    </script>
+</head>
+<body>
+    <p>If you are not redirected automatically, <a href="public/index.php">click here</a>.</p>
+</body>
+</html>
